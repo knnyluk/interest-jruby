000c1e30102ad4741a76642e023fb1d85b8da430
dannybenson <dan.s.benson@gmail.com>
2014-04-10 21:13:13 -0500

    neo4j heroku setup

--- config/initializers/neography.rb
+++ config/initializers/neography.rb
@@ -1,18 +1,15 @@
-Neography.configure do |config|
-  config.protocol           = "http://"
-  config.server             = "localhost"
-  config.port               = 7474
-  config.directory          = ""  # prefix this path with '/'
-  config.cypher_path        = "/cypher"
-  config.gremlin_path       = "/ext/GremlinPlugin/graphdb/execute_script"
-  config.log_file           = "neography.log"
-  config.log_enabled        = false
-  config.slow_log_threshold = 0    # time in ms for query logging
-  config.max_threads        = 20
-  config.authentication     = nil  # 'basic' or 'digest'
-  config.username           = nil
-  config.password           = nil
-  config.parser             = MultiJsonParser
+neo4j_url = ENV["GRAPHENEDB_URL"] || "http://localhost:7474" # default to local server
+
+uri = URI.parse(neo4j_url)
+
+require 'neography'
+
+Neography.configure do |conf|
+  conf.server = uri.host
+  conf.port = uri.port
+  conf.authentication = 'basic'
+  conf.username = uri.user
+  conf.password = uri.password
 end
 
 @neo = Neography::Rest.new
